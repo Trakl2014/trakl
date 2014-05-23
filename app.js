06242b4778e27d49fdd08aaa23aa20c73c27a7ed
Lee <leeblazek@gmail.com>
2014-05-24 08:35:34 +1200

    added UA

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-
 /**
  * Module dependencies.
  */
 
+var UrbanAirshipPush = require('urban-airship-push');
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var submitted = require('./routes/submitted');
 var http = require('http');
 var path = require('path');
 
@@ -25,12 +27,102 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
+app.get('/submitted', submitted.submit);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+//for qt bike fest dev
+// var config = {
+//     key: 'g1XE7nuhSqiJ9Xbb8GIv4w',
+//     secret: 'rV98lhldS4y7HZykNk0UBQ',
+//     masterSecret: '4kPmYtM5Ta206Y2YCdYmFg'
+// };
+
+
+//for qt bike trakl dev
+var config = {
+    key: 'X5diE07QRKCKvxwvIO9lCg',
+    secret: 'DNls_9ffQ6yj8LxO5s1EEw',
+    masterSecret: 'H6NsAzvZRt61VymecApXdg'
+};
+
+
+
+var urbanAirshipPush = new UrbanAirshipPush(config);
+
+// var pushInfo = {
+//     device_types: 'all',
+//     audience: 'all',
+//     notification: {
+//         alert: 'Blubb blub bla'
+//     }
+// };
+
+
+var dataNew;
+
+app.post('/', function(req, res) {
+    var message = req.body.message;
+    // errors = validate(message),
+    res.locals.message = message;
+    // res.redirect('/submitted');
+
+    var pushInfo = {
+        device_types: 'all',
+        audience: 'all',
+        notification: {
+            alert: res.locals.message
+        }
+    };
+
+    function render(isOk) {
+        // res.render('index', locals);
+        res.render('submitted', {
+            title: message,
+            opID: isOk
+        });
+    } 
+    urbanAirshipPush.push.send(pushInfo, function(err, data) {
+
+
+        if (err) {
+            // Handle error
+            return;
+        }
+        res.locals.ok = data.ok;
+        console.log(data);
+        console.log(data + res.locals.ok);
+        console.log(data.ok);
+        // return res.locals.ok;
+        render(res.locals.ok);
+
+    });
+    // if (errors.length === 0) {
+    //     sendEmail(message, function(success) {
+    //         if (!success) {
+    //             // locals.error = 'Error sending message';
+    //             // locals.message = message;
+    //         } else {
+    //             // locals.notice = 'Your message has been sent.';
+    //         }
+    //         render();
+    //     });
+    // } else {
+    //     // locals.error = 'Your message has errors:';
+    //     // locals.errors = errors;
+    //     // locals.message = message;
+    //     render();
+    // }
+    console.log(res.locals.ok)
+
+
+
+
 });
