50bb2159edbfa797de5002b56fca9f6a346fbab7
Don Smith <locksmithdon@gmail.com>
2014-05-24 23:46:15 +1200

    Move test post to routes/index

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var routes = require('./routes');
 var mongoose = require('mongoose');
 var user = require('./routes/user');
 var api = require('./routes/api');
-var submitted = require('./routes/submitted');
-var UrbanAirshipPush = require('urban-airship-push');
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -28,69 +26,4 @@ mongoose.connect('mongodb://localhost:27017/trakl');
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
-});
-
-
-// Keep for testing sanity (existing app)
-/*
-for qt bike fest dev
-var config = {
-    key: 'g1XE7nuhSqiJ9Xbb8GIv4w',
-    secret: 'rV98lhldS4y7HZykNk0UBQ',
-    masterSecret: '4kPmYtM5Ta206Y2YCdYmFg'
-};
-*/
-
-
-//for qt bike trakl dev
-var config = {
-    key: 'X5diE07QRKCKvxwvIO9lCg',
-    secret: 'DNls_9ffQ6yj8LxO5s1EEw',
-    masterSecret: 'H6NsAzvZRt61VymecApXdg'
-};
-
-var dataNew;
-var urbanAirshipPush = new UrbanAirshipPush(config);
-
-app.post('/', function(req, res) {
-
-    var message = req.body.message;
-    // errors = validate(message),
-    res.locals.message = message;
-    // res.redirect('/submitted');
-
-    var pushInfo = {
-        device_types: 'all',
-        audience: 'all',
-        notification: {
-            alert: res.locals.message
-        }
-    };
-
-    function render(isOk) {
-        // res.render('index', locals);
-        res.render('submitted', {
-            title: message,
-            opID: isOk
-        });
-    } 
-
-    urbanAirshipPush.push.send(pushInfo, function(err, data) {
-
-        if (err) {
-            // TODO: Handle error
-            return;
-        }
-
-        res.locals.ok = data.ok;
-        console.log(data);
-        console.log(data + res.locals.ok);
-        console.log(data.ok);
-        // return res.locals.ok;
-        render(res.locals.ok);
-
-    });
-
-    console.log(res.locals.ok)
-
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var UrbanAirshipPush = require('urban-airship-push');
 
 /*
  * GET home page.
@@ -19,6 +20,73 @@ router.get('/submitted', function(req, res) {
     res.render('submitted', {
         title: res.locals.message
     });
+});
+
+
+/*
+ * POST home page.
+ */
+
+router.post('/', function(req, res) {
+
+    // Keep for testing sanity (existing app)
+    /*
+    for qt bike fest dev
+    var config = {
+        key: 'g1XE7nuhSqiJ9Xbb8GIv4w',
+        secret: 'rV98lhldS4y7HZykNk0UBQ',
+        masterSecret: '4kPmYtM5Ta206Y2YCdYmFg'
+    };
+    */
+
+    //for qt bike trakl dev
+    var config = {
+        key: 'X5diE07QRKCKvxwvIO9lCg',
+        secret: 'DNls_9ffQ6yj8LxO5s1EEw',
+        masterSecret: 'H6NsAzvZRt61VymecApXdg'
+    };
+
+    var urbanAirshipPush = new UrbanAirshipPush(config);
+
+    var message = req.body.message;
+    // errors = validate(message),
+    res.locals.message = message;
+    // res.redirect('/submitted');
+
+    var pushInfo = {
+        device_types: 'all',
+        audience: 'all',
+        notification: {
+            alert: res.locals.message
+        }
+    };
+
+    function render(isOk) {
+        // res.render('index', locals);
+        res.render('submitted', {
+            title: message,
+            opID: isOk
+        });
+    } 
+
+    urbanAirshipPush.push.send(pushInfo, function(err, data) {
+
+        if (err) {
+            // TODO: Handle error
+            return;
+        }
+
+        res.locals.ok = data.ok;
+        console.log(data);
+        console.log(data + res.locals.ok);
+        console.log(data.ok);
+        // return res.locals.ok;
+        render(res.locals.ok);
+
+    });
+
+    console.log(res.locals.ok)
+
 });
 
 module.exports = router;
