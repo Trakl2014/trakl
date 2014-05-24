ca62e4e512472ae7ca5280531f2a2f2b09cb807d
Peter Rosser <tharax@gmail.com>
2014-05-25 00:32:41 +1200

    added getJourney function

--- routes/getfromTrafficAPI.js
+++ routes/getfromTrafficAPI.js
@@ -30,3 +30,29 @@ exports.getJourneyList = function (callback) {
 
     });
 };
+
+exports.getJourney = function (ref, callback) {
+    var TrafficOptions = {
+        host: 'trackl-traffic-api.azurewebsites.net',
+        path: '/journey?ref=' + ref
+    };
+    
+    https.get(TrafficOptions, function (TrafficResponse) {
+        var TrafficOptions = '';
+        var trafficData = '';
+
+        TrafficResponse.on('data', function (chunk) {
+            // chunk response from Traffic
+            trafficData += chunk;
+            var message = "" + chunk;
+            console.log('Received response of ' + message.length + ' bytes from Traffic.');
+        });
+
+        TrafficResponse.on('end', function () {
+            // final response, now process data
+            console.log('response end.');
+            callback(trafficData);
+        });
+
+    });
+}
