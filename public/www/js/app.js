b6536ee2d8b2a0739de7591096fe1b28e1f4751e
Lee <leeblazek@gmail.com>
2014-05-25 13:38:13 +1200

    added www to test cors on phone gap app

---
+++ public/www/js/app.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+    var onsenApp = angular.module('myApp', ['onsen.directives']);
+    onsenApp.controller('dataCtrl', function($scope, $http) {
+        $scope.data = [];
+        $scope.sampleResponse_old = {
+            "userId": "24",
+            "journeyRef": "tamaki",
+            "travelMinutes": "34",
+            "isImproving": true
+        }
+
+        // $scope.sampleResponse = {
+        //     __v: 0,
+        //     _id: "5380ba48cd8df6ef3d6436dc",
+        //     isImproving: "true",
+        //     journeyRef: "R05-EB",
+        //     travelMinutes: "26",
+        //     userId: "44"
+        // }
+
+        $scope.trafficData = [];
+        console.log($scope.data)
+        $scope.handleDataLoaded = function(data, status) {
+            $scope.sampleResponse = data;
+            console.log('handle', $scope.sampleResponse, data)
+        }
+        $scope.fetch = function() {
+            $http.get('/api/travel-time?userId=44').success($scope.handleDataLoaded);
+            // $http({
+            //     method: 'GET',
+            //     url: 'javascripts/fieldDemoData2.json'
+            // }).
+            // success($scope.handleDataLoaded).
+            // error(function(data, status, headers, config) {
+            //     // called asynchronously if an error occurs
+            //     // or server returns response with an error status.
+            //     alert('status')
+            // });
+        }
+        $scope.fetch();
+    })
+})();
