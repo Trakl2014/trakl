80fd68fb9a60173378f5c37a5116f4d6135110bc
Lee <leeblazek@gmail.com>
2014-06-07 10:43:58 +1200

    some fixes

--- services/traffic-polling.js
+++ services/traffic-polling.js
@@ -44,14 +44,20 @@ var updateTrafficData = function(journeys) {
 
         traffic.getJourneyTraffic(journey.journeyRef, function(trafficData) {
             if (trafficData.substr(0, 28) !== 'The page cannot be displayed') {
-                var trafficDetails = JSON.parse(trafficData);
-                var isImproving = trafficDetails.minutes < trafficDetails.lastMinutes;
-                var isBetter = isImproving ? 'better' : 'worse';
-                message = messageTemplate.replace('XXXX', trafficDetails.minutes);
-                message = message.replace('YYYY', isBetter);
-                console.log('Message: ' + message);
-                notification.notify(message);
-                saveTraffic(journey.userId, trafficDetails);
+                try {
+                    var trafficDetails = JSON.parse(trafficData);
+                    var isImproving = trafficDetails.minutes < trafficDetails.lastMinutes;
+                    var isBetter = isImproving ? 'better' : 'worse';
+                    message = messageTemplate.replace('XXXX', trafficDetails.minutes);
+                    message = message.replace('YYYY', isBetter);
+                    console.log('Message: ' + message);
+                    notification.notify(message);
+                    saveTraffic(journey.userId, trafficDetails);
+                } catch (e) {
+                    console.log(e);
+                }
+
+
             } else {
                 console.log('No traffic data returned for ref: ' + journey.journeyRef);
             }
