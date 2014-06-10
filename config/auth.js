c1ce872c0065d74b29b4c388946b4e45d5d1900f
Lee <leeblazek@gmail.com>
2014-06-10 19:10:47 +1200

    added twitter keys

--- config/auth.js
+++ config/auth.js
@@ -10,9 +10,9 @@ module.exports = {
     },
 
     'twitterAuth': {
-        'consumerKey': 'your-consumer-key-here',
-        'consumerSecret': 'your-client-secret-here',
-        'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey': 'qxYxRaaAHPMBZqJwlmluWfyxc',
+        'consumerSecret': 'pYQBVuX1gh7e4z5psq5ZmEpIX6Eq0RJIOvYkNgZNCjtHvVYGFr',
+        'callbackURL': '/auth/twitter/callback'
     },
 
     'googleAuth': {
