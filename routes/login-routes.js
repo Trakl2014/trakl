0b5f34be60a43414601614d6e0b219b98976d2c6
Lee <leeblazek@gmail.com>
2014-06-11 16:50:21 +1200

    more css updates to login

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -37,7 +37,7 @@ module.exports = function(app, passport) {
     // LOGOUT ==============================
     app.get('/logout', function(req, res) {
         req.logout();
-        res.redirect('/');
+        res.redirect('/login-home');
     });
 
     // =============================================================================
